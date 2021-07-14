c57a7408c500d9e34671bb589763b4c3e2a9eba3
Asayelfalatah <Asayel.Falatah@ouylook.sa>
2021-07-14 23:26:19 +0300

    2nd sprint
    menu 2 and add files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -64,7 +65,7 @@ public class Main {
                 break;
 
             case 2: // Show File Options menu
-
+                fileOptionsScreen();
 
                 break;
             case 3: //close app
@@ -77,5 +78,88 @@ public class Main {
         }
 
     }
+    private static ArrayList<String> options2 = new ArrayList<String>();
 
+    static void fileOptionsScreen() {
+        options2.clear();
+
+        options2.add("1. Add a File");
+        options2.add("2. Delete A File");
+        options2.add("3. Search A File");
+        options2.add("4. Return to Menu");
+
+        System.out.println("File Options Menu");
+        for (String s : options2) {
+            System.out.println(s);
+        }
+        Scanner in = new Scanner(System.in);
+
+        int returnOption = 0;
+        try {
+            returnOption = in.nextInt();
+        }
+        catch (InputMismatchException ex) {
+
+        }
+        switch(returnOption) {
+
+            case 1: // Add File
+                AddFile();
+
+                fileOptionsScreen();
+                break;
+            case 2: // Delete File
+
+
+                fileOptionsScreen();
+                break;
+            case 3: // Search File
+
+                fileOptionsScreen();
+                break;
+
+            case 4: // Return to Menu
+                welcomeScreen();
+                break;
+
+            default:
+                System.out.println("Invalid Option");
+                fileOptionsScreen();
+                break;
+
+        }
+    }
+    public static void AddFile() {
+
+        File theDir = new File(pathName);
+        if (!theDir.exists()){
+            theDir.mkdirs();
+        }
+
+        System.out.println("Please Enter the Filename:");
+
+        String fileName = getInputString();
+
+        System.out.println("You are adding a file named: " + fileName);
+
+        try {
+            File file = new File(pathName+ fileName);
+
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+
+            } else {
+                System.out.println("This File Already Exits");
+            }
+        }catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    private static String getInputString() {
+
+        Scanner in = new Scanner(System.in);
+        return(in.nextLine());
+
+    }
 }
